4cd62cd5c1767c112d052baefa21d186c5feb54a
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-19 00:13:48 +0530

    Initial commit

---
+++ main.sh
@@ -0,0 +1,18 @@
+ #Using Shell (macOS and Linux):
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+#Using PowerShell (Windows):
+iwr https://deno.land/x/install/install.ps1 -useb | iex
+
+
+#Using Scoop (Windows):
+scoop install deno
+
+#Using Chocolatey (Windows):
+choco install deno
+
+#Using Homebrew (macOS):
+brew install deno
+
+#Using Cargo (Windows, macOS, Linux):
+cargo install deno
